b5acacd222a95901dac06c9c759d560f068cad71
Saksham <saksham.8484@gmail.com>
2022-04-07 22:37:51 +0530

    pass visibility

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,11 +1,21 @@
-import { Link } from 'react-router-dom'
-import { useAuth } from '../../contexts/AuthContext/AuthContext'
-import styles from "./Signin.module.css"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext/AuthContext";
+import styles from "./Signin.module.css";
 
 export const Signin = () => {
-
   const { email, setEmail, password, setPassword, setCredentials } = useAuth();
-  
+  const [passwordType, setType] = useState("password");
+  const [toggleIcon, setToggleIcon] = useState("visibility_off");
+  const togglePassword = () => {
+    if (passwordType === "password") {
+      setType("text");
+      setToggleIcon("visibility");
+    } else {
+      setType("password");
+      setToggleIcon("visibility_off");
+    }
+  };
 
   return (
     <div className={`${styles.signinSignup} videoListing`}>
@@ -19,21 +29,39 @@ export const Signin = () => {
             className="txt"
             placeholder="Johndoe@gmail.com"
             name="email"
-            onChange={e => setEmail(e.target.value)} />
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div className="input-grp">
+        <div className={`${styles.passVis} input-grp`}>
           <label htmlFor="password">Enter your password</label>
           <input
-            type="Password"
+            type={passwordType}
             value={password}
             className="pwd"
             placeholder="Password"
             name="password"
-            onChange={e => setPassword(e.target.value)} />
-          <Link className={`${styles.forgotPassword}`} to="/signup">Forgot password?</Link>
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <i
+            className={`${styles.passwordVisibility} material-icons`}
+            onClick={() => togglePassword()}
+          >
+            {toggleIcon}
+          </i>
+          <div className={`${styles.forgotPassword}`}>
+            Forgot password?
+          </div>
         </div>
-        <input type="submit" value="Sign In" className={`${styles.signInButton}`} onClick={ e => setCredentials(e) } />
+        <input
+          type="submit"
+          value="Sign In"
+          className={`${styles.signInButton}`}
+          onClick={(e) => setCredentials(e)}
+        />
+        <p>
+          Don't have an Account? <Link to="/signup">Sign Up</Link>
+        </p>
       </form>
     </div>
-  )
-}
+  );
+};
