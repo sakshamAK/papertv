4a47fdeef6b812690cc56d5e6e61e2c769d4769a
sakshamak <saksham.8484@gmail.com>
2022-08-22 12:45:02 +0530

    style: responsive home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,12 +14,11 @@ export const Home = () => {
             <div className="videoListing">
                 <h1 className={`${styles.heading}`} >Categories</h1>
                 <div className="videoContainer">
-                    {categoryData.map(({ _id, thumbnail, category, title }) => (
+                    {categoryData.map(({ _id, thumbnail, category }) => (
                         <Link className={`${styles.productLink}`} to={`/${category}`} key={_id}>
                             <div className={`${styles.thumbnailContainer}`}>
-                                <img className="img-resp" src={thumbnail} alt={category} title={title} />
-                                <h4>{title}</h4>
-                                {category !== "" ? <h4>{category}</h4> : ""}
+                                <img className="img-resp" src={thumbnail} alt={category} />
+                                <div className = {`${styles.categTitle}`}><img src = "https://picsum.photos/40/40" />{category !== "" ? <h4>{category}</h4> : ""}</div>
                             </div>
                         </Link>
                     ))}
